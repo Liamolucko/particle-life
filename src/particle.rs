727ff3ae640cb8dc75d3c4c868ab9771a4793eed
Liam Murphy <liampm32@gmail.com>
2021-03-14 12:22:29 +1100

    perf: Don't include particles' velocity when sending to main thread
    This is only a tiny improvement but why not

--- src/particle.rs
+++ src/particle.rs
@@ -4,6 +4,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Particle {
     pub pos: Vector,
+    // This isn't needed for rendering, so don't waste time serializing/deserializing it.
+    #[serde(skip)]
     pub vel: Vector,
     pub r#type: usize,
 }
